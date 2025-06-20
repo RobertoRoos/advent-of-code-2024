493d678d4cd8b62428c861696f164185c5bfee31
RobertoRoos <robert.soor@gmail.com>
2025-06-20 18:46:49 +0300

    Added day 5 part 2

--- src/advent_of_code/day_05/__main__.py
+++ src/advent_of_code/day_05/__main__.py
@@ -1,22 +1,46 @@
 from collections import defaultdict
-from typing import List, Set
+from typing import List, Self, Set
 
 from advent_of_code.shared import Solver, main
 
 
+class Page:
+    """Page entity, so it can be sorted.
+
+    We will rely on the default `sorted` function. We just need to implement a custom
+    comparison method, which will rely on the page order information.
+    """
+
+    ORDER: defaultdict[int, Set[Self]] = defaultdict(set)
+    # Like ``n: [list of numbers that may only follow n, not precede it]``
+
+    def __init__(self, number: int | str):
+        self.number = int(number) if isinstance(number, str) else number
+
+    def __eq__(self, other: Self) -> bool:
+        return self.number == other.number
+
+    def __hash__(self) -> int:
+        return self.number
+
+    def __repr__(self) -> str:
+        return f"<Page ({self.number})>"
+
+    def __lt__(self, other: Self) -> bool:
+        following = self.ORDER[self.number]
+        if other in following:
+            return True  # `other` must be after `self`, so `self` is less than `other`
+
+        return False  # `other` has no relation to `self`, just return false
+
+
 class Day05(Solver):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.page_orders: defaultdict[int, Set[int]] = defaultdict(set)
-        # Like ``n: [list of numbers that may only follow n, not precede it]``
 
     def __call__(self) -> str:
 
         do_updates = False
 
-        updates: List[List[int]] = []
+        updates: List[List[Page]] = []
 
         for line in self.iterate_input():
             line = line.strip()
@@ -25,38 +49,55 @@ class Day05(Solver):
                 continue
 
             if not do_updates:
-                number_1, _, number_2 = line.partition("|")
-                number_1, number_2 = int(number_1), int(number_2)
-                self.page_orders[int(number_1)].add(int(number_2))
+                page_1, _, page_2 = line.partition("|")
+                page_1, page_2 = Page(page_1), Page(page_2)
+                Page.ORDER[page_1.number].add(page_2)
 
             else:
-                numbers = [int(t) for t in line.split(",")]
-                updates.append(numbers)
+                pages = [Page(t) for t in line.split(",")]
+                updates.append(pages)
 
-        value = 0
+        value_part_1 = 0
+        updates_invalid: List[List[Page]] = []
 
         for update in updates:
             if self.check_update_order(update):
-                middle_idx = int((len(update) - 1) / 2)
-                value += update[middle_idx]
+                value_part_1 += self.get_middle(update).number
+            else:
+                updates_invalid.append(update)
 
-        return str(value)
+        if self.args.part == 1:
+            return str(value_part_1)
 
-    def check_update_order(self, update: List[int]) -> bool:
+        value_part_2 = 0
+
+        for update in updates_invalid:
+            update_sorted = sorted(update)
+            value_part_2 += self.get_middle(update_sorted).number
+
+        return str(value_part_2)
+
+    @staticmethod
+    def get_middle(array: List[Page]) -> Page:
+        middle_idx = int((len(array) - 1) / 2)
+        return array[middle_idx]
+
+    @staticmethod
+    def check_update_order(update: List[Page]) -> bool:
         """Return True if an update (i.e. set of pages) is in a valid order.
 
         Loop over each number and than over all the numbers after. For each pair find
         the relevant rules and verify those rules.
         """
 
-        for i, number in enumerate(update):
+        for i, page in enumerate(update):
 
-            for other_number in update[(i + 1) :]:
-                following_numbers = self.page_orders[other_number]
+            for other_page in update[(i + 1) :]:
+                following_pages = Page.ORDER[other_page.number]
 
                 # If the first number must actually follow the second number, a rule
                 # is violated and this is not a valid update:
-                if number in following_numbers:
+                if page in following_pages:
                     return False
 
         return True
--- tests/day_05/test_day_05.py
+++ tests/day_05/test_day_05.py
@@ -1,6 +1,6 @@
 import unittest
 
-from advent_of_code.day_05.__main__ import Day05
+from advent_of_code.day_05.__main__ import Day05, Page
 
 from ..advent_testcase import AdventTestCase
 
@@ -8,6 +8,9 @@ from ..advent_testcase import AdventTestCase
 class TestDay05(AdventTestCase):
 
     PUZZLE = Day05
+
+    def setUp(self):
+        Page.ORDER.clear()
 
     def test_sample_part_1(self):
         solver = self.get_solver(1)
@@ -17,7 +20,18 @@ class TestDay05(AdventTestCase):
     def test_sample_part_2(self):
         solver = self.get_solver(2)
         result = solver()
-        self.assertEqual("", result)
+        self.assertEqual("123", result)
+
+    def test_page_hash(self):
+        my_set = {Page(10), Page(15), Page(9)}
+        self.assertTrue(Page(15) in my_set)
+
+    def test_page_sort(self):
+        Page.ORDER[9] = {Page(5), Page(2)}
+        Page.ORDER[5] = {Page(2), Page(1)}
+        Page.ORDER[4] = {Page(2)}
+        self.assertTrue(Page(9) < Page(5))
+        self.assertTrue(Page(5) < Page(2))
 
 
 if __name__ == "__main__":
