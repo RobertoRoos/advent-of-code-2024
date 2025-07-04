34d9ada1e761074852a1bbb4e37707edc8228d55
RobertoRoos <robert.soor@gmail.com>
2025-07-04 15:59:23 +0200

    Replaced d7p2 to use recursion, running is about 0.03 seconds now

--- src/advent_of_code/day_07/__main__.py
+++ src/advent_of_code/day_07/__main__.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from typing import List
+from typing import List, Tuple
 
 from advent_of_code.shared import Solver, main
 
@@ -40,51 +40,71 @@ class Day07(Solver):
         return str(result)
 
     @classmethod
-    def possible_equation(cls, total: int, numbers: List[int]) -> bool:
-        """Check if this equation can be completed."""
-        size = len(numbers) - 1  # Number of operators
-        base = len(cls.OPTIONS)
-        for i in range(base**size):  # Systematically try all options:
-            i_based_str = cls.base_string(i, base)
-            i_based_str = (size - len(i_based_str)) * "0" + i_based_str
-            operators = [Operator.from_number(c) for c in i_based_str]
+    def possible_equation(
+        cls,
+        total: int,
+        numbers: List[int],
+        operators: List[Operator] | None = None,
+        total_backwards: int = 0,
+    ) -> bool:
+        """Recursively check the possibility of an equation.
 
-            if cls.test_multiple_operations(total, numbers, operators):
+        Each execution branch will trigger new child execution branches, until the set
+        of operands is full and it finally returns.
+        We check the opposite action of each operation to see if we need to keep pursuing
+        a particular branch. E.g. for "190: 10 19" we could never use concatenate to make
+        the answer, regardless of what comes before.
+
+        :param total:
+        :param numbers:
+        :param operators:   Recursive element
+                            These are the last operators of this branch of execution
+        :param total_backwards: Result with the operators we have already
+        """
+        if operators is None:
+            operators = []
+            total_backwards = total
+
+        progress = len(operators)
+
+        if progress == len(numbers) - 1:
+            return numbers[0] == total_backwards
+
+        if progress >= len(numbers):
+            raise ValueError("Too many operators for this set of numbers!")
+
+        # Not yet done, figure out the next possible operators, by working
+        # the sum backwards:
+        next_options: List[Tuple[Operator, int]] = []
+
+        next_number_idx = len(numbers) - 1 - progress  # Number for the next operator
+        next_number = numbers[next_number_idx]
+
+        if Operator.CONCAT in cls.OPTIONS:
+            total_backwards_str = str(total_backwards)
+            next_number_str = str(next_number)
+            if total_backwards != next_number and total_backwards_str.endswith(
+                next_number_str
+            ):
+                total_backwards_next_str = total_backwards_str[: -len(next_number_str)]
+                next_options.append((Operator.CONCAT, int(total_backwards_next_str)))
+
+        if Operator.MULT in cls.OPTIONS:
+            if total_backwards % next_number == 0:
+                next_options.append((Operator.MULT, int(total_backwards / next_number)))
+
+        if Operator.ADD in cls.OPTIONS:
+            if total_backwards > next_number:
+                next_options.append((Operator.ADD, total_backwards - next_number))
+
+        for option, next_total_backwards in next_options:
+            next_operations = [option] + operators[:]
+            if cls.possible_equation(
+                total, numbers, next_operations, next_total_backwards
+            ):
                 return True
 
         return False
-
-    @classmethod
-    def test_multiple_operations(
-        cls, expected: int, numbers: List[int], ops: List[Operator]
-    ) -> int:
-        """Return true if this operation series matches the expectation."""
-        total = numbers[0]
-        for number, op in zip(numbers[1:], ops):
-            total = cls.do_operation(total, number, op)
-
-            if total > expected:
-                return False  # No need to continue, already done
-
-        return total == expected
-
-    @staticmethod
-    def do_operation(number_1: int, number_2: int, op: Operator) -> int:
-        if op == Operator.ADD:
-            return number_1 + number_2
-        if op == Operator.MULT:
-            return number_1 * number_2
-        return int(str(number_1) + str(number_2))
-
-    @staticmethod
-    def base_string(n, base) -> str:
-        if n == 0:
-            return "0"
-        nums = []
-        while n:
-            n, r = divmod(n, base)
-            nums.append(str(r))
-        return "".join(reversed(nums))
 
 
 if __name__ == "__main__":
