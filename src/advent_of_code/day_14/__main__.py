65eb2c05d5965c74d44c4e9d6dd9a3340e8f9a4e
RobertoRoos <robert.soor@gmail.com>
2025-07-29 18:45:48 +0200

    Added d14p2 - And I hated every part of it

--- src/advent_of_code/day_14/__main__.py
+++ src/advent_of_code/day_14/__main__.py
@@ -1,16 +1,18 @@
 from typing import List
 
-from advent_of_code.shared import RowCol, Solver, main
+from advent_of_code.shared import Grid, GridItem, RowCol, Solver, main
 
 
-class Robot:
+class Robot(GridItem):
     """Helper class to store info for each robot."""
 
     ROWS = 103
     COLS = 101
 
     def __init__(self):
-        self.position = RowCol(0, 0)
+        super().__init__()
+        self.loc = RowCol(0, 0)
+        self.character = "x"
         self.velocity = RowCol(0, 0)
 
     @staticmethod
@@ -25,21 +27,21 @@ class Robot:
             # y = row
 
         robot = Robot()
-        robot.position = values[0]
+        robot.loc = values[0]
         robot.velocity = values[1]
 
         return robot
 
     def __repr__(self) -> str:
-        return f"Robot({self.position.row}, {self.position.col})"
+        return f"Robot({self.loc.row}, {self.loc.col})"
 
     def move(self, steps: int = 1):
         """Update position based on velocity for a numer of steps."""
-        row = self.position.row + steps * self.velocity.row
-        col = self.position.col + steps * self.velocity.col
+        row = self.loc.row + steps * self.velocity.row
+        col = self.loc.col + steps * self.velocity.col
         row = row % self.ROWS  # Mimic teleporting:
         col = col % self.COLS
-        self.position = RowCol(row=row, col=col)
+        self.loc = RowCol(row=row, col=col)
 
 
 class Day14(Solver):
@@ -48,26 +50,61 @@ class Day14(Solver):
 
         robots: List[Robot] = [Robot.from_str(line) for line in self.iterate_input()]
 
-        for robot in robots:
-            robot.move(100)
+        if self.args.part == 1:
+            for robot in robots:
+                robot.move(100)
 
-        quadrants: List[int] = [0, 0, 0, 0]
-        q1_row = (Robot.ROWS - 1) / 2
-        q1_col = (Robot.COLS - 1) / 2
+            quadrants: List[int] = [0, 0, 0, 0]
+            q1_row = (Robot.ROWS - 1) / 2
+            q1_col = (Robot.COLS - 1) / 2
 
-        for robot in robots:
-            if robot.position.row < q1_row and robot.position.col < q1_col:
-                quadrants[0] += 1
-            elif robot.position.row < q1_row and robot.position.col > q1_col:
-                quadrants[1] += 1
-            elif robot.position.row > q1_row and robot.position.col < q1_col:
-                quadrants[2] += 1
-            elif robot.position.row > q1_row and robot.position.col > q1_col:
-                quadrants[3] += 1
+            for robot in robots:
+                if robot.loc.row < q1_row and robot.loc.col < q1_col:
+                    quadrants[0] += 1
+                elif robot.loc.row < q1_row and robot.loc.col > q1_col:
+                    quadrants[1] += 1
+                elif robot.loc.row > q1_row and robot.loc.col < q1_col:
+                    quadrants[2] += 1
+                elif robot.loc.row > q1_row and robot.loc.col > q1_col:
+                    quadrants[3] += 1
 
-        score = quadrants[0] * quadrants[1] * quadrants[2] * quadrants[3]
+            score = quadrants[0] * quadrants[1] * quadrants[2] * quadrants[3]
+
+        else:
+            # I read by other solutions that when the Christmas trees shows, all roots
+            # are in a unique position. So we just search for that instead.
+            seconds = 0
+            while True:
+                for r in robots:
+                    r.move()
+                seconds += 1
+
+                if self.positions_are_unique(robots):
+                    # self.print(robots)
+                    break
+            score = seconds
 
         return str(score)
+
+    @staticmethod
+    def positions_are_unique(robots: List[Robot]) -> bool:
+        unique_positions = set()
+        for r in robots:
+            if r.loc in unique_positions:
+                return False
+            unique_positions.add(r.loc)
+
+        return True
+
+    @staticmethod
+    def print(robots: List[Robot]):
+        grid = Grid()
+        grid.rows = Robot.ROWS
+        grid.cols = Robot.COLS
+        for r in robots:
+            grid.items[r.loc] = r
+
+        grid.print()
 
 
 if __name__ == "__main__":
--- tests/day_14/test_day_14.py
+++ tests/day_14/test_day_14.py
@@ -16,25 +16,22 @@ class TestDay14(AdventTestCase):
 
     def test_move(self):
         robot = Robot.from_str("p=2,4 v=2,-3")
-        self.assertEqual(RowCol(row=4, col=2), robot.position)
+        self.assertEqual(RowCol(row=4, col=2), robot.loc)
         robot.move()
-        self.assertEqual(RowCol(row=1, col=4), robot.position)
+        self.assertEqual(RowCol(row=1, col=4), robot.loc)
         robot.move()
-        self.assertEqual(RowCol(row=5, col=6), robot.position)
+        self.assertEqual(RowCol(row=5, col=6), robot.loc)
 
         robot = Robot.from_str("p=2,4 v=2,-3")
         robot.move(5)
-        self.assertEqual(RowCol(row=3, col=1), robot.position)
+        self.assertEqual(RowCol(row=3, col=1), robot.loc)
 
     def test_sample_part_1(self):
         solver = self.get_solver(1)
         result = solver()
         self.assertEqual("12", result)
 
-    # def test_sample_part_2(self):
-    #     solver = self.get_solver(2)
-    #     result = solver()
-    #     self.assertEqual("xxx", result)
+    # Nothing to test for part 2
 
 
 if __name__ == "__main__":
