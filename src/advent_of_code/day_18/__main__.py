ff7eb3a5da126c6acb7e17c73ee6c11e129a3148
RobertoRoos <robert.soor@gmail.com>
2025-09-15 16:52:12 +0300

    Trying to speed up d18p2, but no success yet

--- src/advent_of_code/day_18/__main__.py
+++ src/advent_of_code/day_18/__main__.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Tuple
+from typing import Dict, List, Tuple, Set
 
 from advent_of_code.shared import (
     Direction,
@@ -53,9 +53,7 @@ class Day18(Solver):
                 if path_so_far is not None and obstacle.loc not in path_so_far:
                     continue  # No need to check validity, already walking around it
 
-                try:
-                    _, path_so_far = self.find_shorest_path(start, goal)
-                except RuntimeError:
+                if not self.check_possible_path(start, goal):
                     return f"{obstacle.loc.col},{obstacle.loc.row}"  # X,Y vs row,col
 
             raise RuntimeError("Couldn't find limiting block!")
@@ -80,12 +78,8 @@ class Day18(Solver):
             # Investigate next paths:
             for direction in Direction:
                 next_tip = this_tip.next(direction)
-                if not self.grid.in_range(next_tip):
-                    continue  # Out of bounds
-
-                if next_tip in self.grid.items:
-                    if self.grid.items[next_tip].character == "#":
-                        continue  # Wall, skip this one
+                if not self.walkable(next_tip):
+                    continue
 
                 next_distance = this_distance + 1
 
@@ -97,6 +91,39 @@ class Day18(Solver):
                     path_queue.push(next_distance, this_path[:] + [next_tip])
 
         raise RuntimeError("Failed to find solution")
+
+    def check_possible_path(self, start: RowCol, goal: RowCol) -> bool:
+        """Simplified pathfinder - return False if no path exists."""
+        reachable_tiles: Set[RowCol] = set()
+        tips: Set[RowCol] = {start}  # "Boundary" of new locations
+
+        while tips:
+            reachable_tiles |= tips
+
+            new_tips: Set[RowCol] = set()
+
+            for tip in tips:
+                for direction in Direction:
+                    new_tip = tip.next(direction)
+                    if not self.walkable(new_tip):
+                        continue
+
+                    if new_tip not in reachable_tiles:
+                        new_tips.add(new_tip)
+
+            tips = new_tips
+
+        return goal in reachable_tiles
+
+    def walkable(self, loc: RowCol) -> bool:
+        if not self.grid.in_range(loc):
+            return False  # Out of bounds
+
+        if loc in self.grid.items:
+            if self.grid.items[loc].character == "#":
+                return False  # Wall, skip this one
+
+        return True
 
     def print_path(self, path: Path):
         path_grid = Grid()
