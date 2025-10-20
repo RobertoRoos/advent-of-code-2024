878bf30515961e8e7c7471f6ff22306401ca7b45
RobertoRoos <robert.soor@gmail.com>
2025-10-20 17:23:35 +0200

    Added d25p1

---
+++ tests/day_25/test_day_25.py
@@ -0,0 +1,24 @@
+import unittest
+
+from advent_of_code.day_25.__main__ import Day25
+
+from ..advent_testcase import AdventTestCase
+
+
+class TestDay25(AdventTestCase):
+
+    PUZZLE = Day25
+
+    def test_sample_part_1(self):
+        solver = self.get_solver(1)
+        result = solver()
+        self.assertEqual("3", result)
+
+    # def test_sample_part_2(self):
+    #     solver = self.get_solver(2)
+    #     result = solver()
+    #     self.assertEqual("xxx", result)
+
+
+if __name__ == "__main__":
+    unittest.main()
